f91ad80121b842a8ddef47bbd298703fefd33f27
Marcel <116410849+MFGPerez@users.noreply.github.com>
2023-05-24 13:08:31 -0600

    Add files via upload

---
+++ assignment-0-MFGPerez/src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author mfgperez
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // hello
+        
+        
+       String msg = "Hello_World"; 
+       System.out.println(msg);
+    }
+    
+}
